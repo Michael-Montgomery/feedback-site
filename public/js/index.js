983060dc99b64c9d937e767afd1ecd7cb683e319
Michael Montgomery <montgomery.michael@hotmail.com>
2016-12-05 13:23:36 -0500

    toer down almost everything. redesigned and built out templating functionality.

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,31 @@
-var app = angular.module('feedback', [
-    'ngRoute',
-    'feedback.welcome',
-    'feedback.viewFeedback',
-    'feedback.newFeedback',
-]);
+var app = angular.module('feedback', []);
 
-app.config(function($routeProvider) {
-    $routeProvider.when('/', {
-        redirectTo: '/welcome'
-    }).otherwise({
-        redirectTo: '/welcome'
+app.controller('feedbackController', function($scope) {
+
+    $(window).scroll(function() {
+
+        var eTop = $('#idea-wrapper').offset().top;
+        var pixels = eTop - $(window).scrollTop()
+        console.log(pixels);
+
+
+        if($(window).scrollTop() === 0) {
+            $('.header').css('background-color', 'transparent').css('opacity', '1');
+            $('.header nav a input, .header nav a button').css('display', 'none')
+        } else if($(window).scrollTop() > 0 && pixels > 0) {
+            $('.header').css('background-color', 'black').css('opacity', '.7');
+            $('.header nav a input, .header nav a button').css('display', 'none');
+        } else {
+            $('.header').css('background-color', 'black').css('opacity', '1');
+            $('.header nav a input, .header nav a button').css('display', 'inline')
+        }
     })
-});
+
+
+
+
+
+    $scope.ideas = [
+        1,2,3,4,5,6
+    ]
+})
